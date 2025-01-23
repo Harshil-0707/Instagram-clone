d23cc3097ac392f536da24e9a28b9a23a515f469
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2025-01-24 01:05:24 +0530

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,71 +14,107 @@ import {
   InstagramIcon,
 } from "../assets";
 
+const navLinkOne = [
+  { img: Home, text: "Home" },
+  { img: Search, text: "Search" },
+  { img: Explore, text: "Explore" },
+  { img: Reels, text: "Reels" },
+  { img: Message, text: "Messages" },
+  { img: Heart, text: "Notifications" },
+  { img: Create, text: "Create" },
+  { img: Home, text: "Profile" },
+];
+
+const navLinkTwo = [
+  { img: Home, text: "AI Studio" },
+  { img: Threads, text: "Threads" },
+  { img: Menu, text: "More" },
+];
+
 export default function Navbar() {
-  const [navLinkOne, _] = React.useState([
-    { img: Home, text: "Home" },
-    { img: Search, text: "Search" },
-    { img: Explore, text: "Explore" },
-    { img: Reels, text: "Reels" },
-    { img: Message, text: "Messages" },
-    { img: Heart, text: "Notifications" },
-    { img: Create, text: "Create" },
-    { img: Home, text: "Profile" },
-  ]);
-  const [navLinkTwo, __] = React.useState([
-    { img: Home, text: "AI Studio" },
-    { img: Threads, text: "Threads" },
-    { img: Menu, text: "More" },
-  ]);
   return (
     <>
-      <div className="md:sticky md:top-0 md:border-r-[1px] md:h-[100vh] max-md:h-30 flex flex-row fixed bottom-0 md:flex-col gap-y-5 md:pt-10 px-3 md:pb-9 border-[#51515187] w-[21%] max-xl:w-20 max-md:w-full max-xl:items-center max-xl:justify-evenly">
-        <div className="mb-2 px-4">
-          <img src={Name} alt="icon" className="block max-xl:hidden" />
-          <img src={InstagramIcon} alt="icon" className="hidden max-xl:block" />
-        </div>
-        <div className="h-full flex flex-col justify-between  max-md:flex-row">
-          <div className="flex flex-col max-md:flex-row">
-            {navLinkOne.map((nav, index) => {
-              return (
-                <NavLink
-                  key={nav.text}
-                  className="hover:bg-[rgba(255,255,255,0.1)] rounded-lg flex items-center py-4 max-xl:flex-col"
+      <DesktopNavbar />
+      <MobileNavbar />
+    </>
+  );
+}
+
+function DesktopNavbar() {
+  return (
+    <div className="md:sticky md:top-0 md:border-r-[1px] md:h-[100vh] max-md:h-30 flex flex-row fixed bottom-0 md:flex-col gap-y-5 md:pt-10 px-3 md:pb-9 border-[#51515187] w-[21%] max-xl:w-20 max-md:hidden max-md:w-full max-xl:items-center max-xl:justify-evenly">
+      <div className="mb-2 px-4">
+        <img src={Name} alt="icon" className="block max-xl:hidden" />
+        <img src={InstagramIcon} alt="icon" className="hidden max-xl:block" />
+      </div>
+      <div className="h-full flex flex-col justify-between  max-md:flex-row">
+        <div className="flex flex-col max-md:flex-row">
+          {navLinkOne.map((nav, index) => {
+            return (
+              <NavLink
+                key={nav.text}
+                className="hover:bg-[rgba(255,255,255,0.1)] flex items-center py-4 max-xl:flex-col"
+              >
+                <img
+                  src={nav.img}
+                  alt="img"
+                  className="pl-3 pr-4 max-xl:px-2"
+                />
+                <span
+                  className={`max-xl:hidden ${
+                    index === 0 ? "font-bold" : "font-normal"
+                  }`}
                 >
-                  <img
-                    src={nav.img}
-                    alt="img"
-                    className="pl-3 pr-4 max-xl:px-2"
-                  />
-                  <span
-                    className={`max-xl:hidden ${
-                      index === 0 ? "font-bold" : "font-normal"
-                    }`}
-                  >
-                    {nav.text}
-                  </span>
-                </NavLink>
-              );
-            })}
-          </div>
-          <div className="flex flex-col max-md:flex-row max-md:items-center">
-            {navLinkTwo.map((nav) => {
-              return (
-                <NavLink
-                  key={nav.text}
-                  className="hover:bg-[rgba(255,255,255,0.1)] rounded-md flex items-center py-4 max-xl:flex-col"
-                >
-                  <img
-                    src={nav.img}
-                    alt="img"
-                    className="pl-3 pr-4 max-xl:px-2"
-                  />
-                  <span className="max-xl:hidden">{nav.text}</span>
-                </NavLink>
-              );
-            })}
-          </div>
+                  {nav.text}
+                </span>
+              </NavLink>
+            );
+          })}
         </div>
+        <div className="flex flex-col max-md:flex-row max-md:items-center">
+          {navLinkTwo.map((nav) => {
+            return (
+              <NavLink
+                key={nav.text}
+                className="hover:bg-[rgba(255,255,255,0.1)] rounded-md flex items-center py-4 max-xl:flex-col"
+              >
+                <img
+                  src={nav.img}
+                  alt="img"
+                  className="pl-3 pr-4 max-xl:px-2"
+                />
+                <span className="max-xl:hidden">{nav.text}</span>
+              </NavLink>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function MobileNavbar() {
+  const navLink = [
+    { img: Home, text: "Home" },
+    { img: Explore, text: "Explore" },
+    { img: Reels, text: "Reels" },
+    { img: Create, text: "Create" },
+    { img: Message, text: "Messages" },
+    { img: Home, text: "Profile" },
+  ];
+  return (
+    <>
+      <div className="h-30 fixed bottom-0 gap-y-5 bg-black px-3 border-t border-[#51515187] flex justify-evenly w-full md:hidden">
+        {navLink.map((nav) => {
+          return (
+            <NavLink
+              key={nav.text}
+              className="hover:bg-[rgba(255,255,255,0.1)] flex items-center py-4"
+            >
+              <img src={nav.img} alt="img" className="pl-3 pr-4" />
+            </NavLink>
+          );
+        })}
       </div>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Name, Search, Heart, InstagramIcon } from "../assets";
 import { Posts, SuggestedAcc } from "../components";
 
 export default function Home() {
@@ -15,26 +16,49 @@ export default function Home() {
 
   return (
     <>
-      <div className="flex justify-center w-full">
-        <div className="flex flex-col">
-          <div className="flex gap-x-5 mb-6 mt-6  justify-center 2xl:ml-[4rem] max-2xl:ml-[0rem]">
-            {story.map((val) => (
-              <div
-                className="flex flex-col items-center relative hover:cursor-pointer"
-                key={val.id}
-              >
-                <div className="absolute h-[60px] w-[60px] -z-10 top-[-1px] rounded-full bg-gradient-to-r from-pink-500 via-purple-500 to-orange-500" />
-                <div
-                  className="h-[58px] w-[58px] mb-1 rounded-full bg-slate-200 border-[2px] border-black"
-                  key={val.id}
-                ></div>
-                <div className="text-sm">{val.name}</div>
-              </div>
-            ))}
+      <div className="w-full flex flex-col">
+        <div className="md:hidden sticky z-50 top-0 h-[60px] px-5 bg-black border-b border-[#51515187] w-full overflow-hidden flex items-center justify-between">
+          <div className="mt-1">
+            <img src={Name} alt="instagram" className="h-8" />
           </div>
-          <Posts />
+          <div className="relative flex items-center justify-evenly gap-x-3 h-[70%] w-[45%]">
+            <input
+              type="text"
+              className="h-[90%] !border-none w-full max-w-[16rem] rounded-lg bg-[rgb(54,54,54)] placeholder:text-[rgb(54,54,54)] pl-5"
+              placeholder="Search"
+            />
+            <span className="absolute left-[2.8rem] text-[rgb(168,168,168)]">
+              Search
+            </span>
+            <img
+              src={Search}
+              alt="Search"
+              className="absolute top-3 h-[0.9rem] left-[1.2rem]"
+            />
+            <img src={Heart} alt="like" />
+          </div>
         </div>
-        <SuggestedAcc />
+        <div className="flex justify-center w-full overflow-hidden">
+          <div className="flex flex-col">
+            <div className="flex gap-x-5 mb-6 mt-6 justify-center 2xl:ml-[4rem] max-2xl:ml-[0rem]">
+              {story.map((val) => (
+                <div
+                  className="flex flex-col items-center relative hover:cursor-pointer"
+                  key={val.id}
+                >
+                  <div className="absolute h-[60px] w-[60px] -z-10 top-[-1px] rounded-full bg-gradient-to-r from-pink-500 via-purple-500 to-orange-500" />
+                  <div
+                    className="h-[58px] w-[58px] mb-1 rounded-full bg-slate-200 border-[2px] border-black"
+                    key={val.id}
+                  ></div>
+                  <div className="text-sm">{val.name}</div>
+                </div>
+              ))}
+            </div>
+            <Posts />
+          </div>
+          <SuggestedAcc />
+        </div>
       </div>
     </>
   );
